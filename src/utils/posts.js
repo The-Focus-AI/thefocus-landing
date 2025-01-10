c0f18b5877dcf9818a16f4de070f2955e3af07c1
Will Schenk <wschenk@gmail.com>
2025-01-10 05:52:24 -0500

    Homepage tweaks

--- src/utils/posts.js
+++ src/utils/posts.js
@@ -17,3 +17,24 @@ export async function getPosts() {
 
   return posts;
 }
+
+export async function getRecentPosts() {
+  let posts = await getCollection("posts");
+  // Sort posts by date
+  posts.sort(
+    (a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime()
+  );
+
+  // Filter out posts without a date or where date is not a real date
+  posts = posts.filter(
+    (post) => post.data.date && !isNaN(new Date(post.data.date).getTime())
+  );
+
+  // Filter out posts that are not published
+  posts = posts.filter((post) => post.data.published);
+
+  // Get the first 2 posts
+  posts = posts.slice(0, 2);
+
+  return posts;
+}
